5efe04b538b230e6d1d84a21d513d1f143380974
Ткаченко Никита Сергеевич <nstkachenko.edu.hse.ru>
2021-06-10 17:15:29 +0300

    finish filter

---
+++ cmake-build-debug/CMakeFiles/Filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Filter.dir/main.cpp.o"
+  "Filter"
+  "Filter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
